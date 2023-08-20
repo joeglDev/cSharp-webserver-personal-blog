87a9ac8c36c9bf172fb8ac91736c4c2a710cbe83
hiroji <joegilbertdev@outlook.com>
2023-08-20 11:39:37 +0100

    feat/BlogPost(likes)
    Added like objects tracking username to BlogPostItem

--- Context/BlogPostContext.cs
+++ Context/BlogPostContext.cs
@@ -4,6 +4,7 @@ namespace Blog.Models;
 public class BlogPostContext : DbContext
 {
      public  DbSet<BlogPostItem>? BlogPostItems { get; set; } 
+      public DbSet<LikedByItem>? LikedByItems { get; set; }
 
     public BlogPostContext(DbContextOptions<BlogPostContext> options)
         : base(options)
@@ -21,9 +22,16 @@ public class BlogPostContext : DbContext
         if (!BlogPostItems.Any())
         {
 
-            BlogPostItem huskys = new BlogPostItem(0, "Hiroji", "Why are Huskys so cute!", "Huskies are often considered adorable due to their distinct features and charming personalities. With their striking blue or multicolored eyes, thick coats, and expressive faces, huskies possess a natural allure that captures the hearts of many. Their fluffy appearance, combined with their playful and friendly nature, creates an instant appeal. Whether it's their mischievous antics, their infectious energy, or their affectionate demeanor, huskies possess a certain charm that brings joy and warmth to those who encounter them. Their cuteness goes beyond their physical attributes and resonates with their spirited and endearing personalities, making them irresistible to many animal lovers.", 0);
+            BlogPostItem huskys = new BlogPostItem(0, "Hiroji", "Why are Huskys so cute!", "Huskies are often considered adorable due to their distinct features and charming personalities. With their striking blue or multicolored eyes, thick coats, and expressive faces, huskies possess a natural allure that captures the hearts of many. Their fluffy appearance, combined with their playful and friendly nature, creates an instant appeal. Whether it's their mischievous antics, their infectious energy, or their affectionate demeanor, huskies possess a certain charm that brings joy and warmth to those who encounter them. Their cuteness goes beyond their physical attributes and resonates with their spirited and endearing personalities, making them irresistible to many animal lovers.");
+            //add timeStamp
             huskys.TimeStamp = DateTime.Now;
+            //add fake like
+            List<LikedByItem> NewLikes = new List<LikedByItem>();
+            NewLikes.Add(new LikedByItem("Alpha"));
+             NewLikes.Add(new LikedByItem("Beta"));
+            huskys.Likes = NewLikes;
 
+            //add post to list
             BlogPostItems.Add(huskys);
             SaveChanges();
         }
--- Models/BlogPost.cs
+++ Models/BlogPost.cs
@@ -1,14 +1,33 @@
 namespace Blog.Models;
 
+
+ public class LikedByItem
+    {
+        public int LikedByItemId { get; set; } // Primary key
+      //  public int UserId { get; set; }
+        public string? UserName { get; set; }
+
+          // Parameterless constructor for Entity Framework
+    public LikedByItem()
+    {
+    }
+
+        public LikedByItem(string name) {
+          UserName = name;
+        }
+    }
+
 public class BlogPostItem
 {
+
     public int Id { get; set; }
     public string Author { get; set; } = "unassigned author";
     public string Title { get; set; } = "unassigned title";
     public string Content { get; set; } = "unassigned content";
-    public int Likes { get; set; }
 
     public DateTime TimeStamp {get; set;}
+
+   public List<LikedByItem> Likes {get; set;} = new List<LikedByItem>();
 
       // Parameterless constructor for Entity Framework
     public BlogPostItem()
@@ -16,13 +35,12 @@ public class BlogPostItem
     }
 
     // Constructor
-    public BlogPostItem(int id, string author, string title, string content, int likes)
+    public BlogPostItem(int id, string author, string title, string content)
     {
         Id = id;
         Author = author;
         Title = title;
         Content = content;
-        Likes = likes;
     }
 
 }
