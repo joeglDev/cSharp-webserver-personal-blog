56bb531397513bfbb78917ab5e70be8e5a78811f
Joe Gilbert <joegilbertdev@outlook.com>
2024-09-01 14:46:21 +0100

    2-get-blog-posts
    refactor

--- v2/controllers/BlogPostService.cs
+++ v2/controllers/BlogPostService.cs
@@ -5,11 +5,11 @@ using Webserver.Models;
 
 public class BlogPostService {
     public static List<BlogPost> GetAllPosts() {
-        List<BlogPost> AllBlogPosts = new List<BlogPost>();
+        List<BlogPost> AllBlogPosts = [];
 
         // note
         // replace this with a database query fetching the post from the database
-        BlogPost examplePost = new BlogPost(1, "Hiroji", "Cats are cool!", "This is an example blog post to tell you that my cat Bennet is one cool cat. A tuxedo cat to be specific! ^w^", DateTime.Now, 5);
+        BlogPost examplePost = new(1, "Hiroji", "Cats are cool!", "This is an example blog post to tell you that my cat Bennet is one cool cat. A tuxedo cat to be specific! ^w^", DateTime.Now, 5);
         AllBlogPosts.Add(examplePost);
 
         return AllBlogPosts;
