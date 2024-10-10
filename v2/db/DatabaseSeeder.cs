47f986246dd67e37d000f26dea95762ba759eb99
Joe Gilbert <joegilbertdev@outlook.com>
2024-10-10 20:26:26 +0100

    2-get-blog-posts
    inserts row if no rows

--- v2/db/DatabaseSeeder.cs
+++ v2/db/DatabaseSeeder.cs
@@ -17,6 +17,7 @@ public class DatabaseSeeder() : DatabaseAbstract
             await _connection.OpenAsync();
 
             await CreateBlogPostTableAsync();
+            await InsertDataIfBlogpostTableEmpty();
             
             Console.WriteLine("Seeded database successfully");
         } catch (Exception Ex) {
@@ -40,6 +41,27 @@ public class DatabaseSeeder() : DatabaseAbstract
         _connection);
 
     await cmd.ExecuteNonQueryAsync();
+    }
+
+    private async Task InsertDataIfBlogpostTableEmpty() {
+        string insertQuery = @"INSERT INTO blogposts (Author, Title, Content, TimeStamp, Likes)
+SELECT :author, :title, :content, :timestamp, :likes
+WHERE NOT EXISTS (
+    SELECT 1 FROM blogposts WHERE Author = :author AND Title = :title
+);";
+    
+        using var cmd = new NpgsqlCommand(insertQuery, _connection);
+        
+        // Set parameter values
+        DateTime now = DateTime.Now;
+
+        cmd.Parameters.AddWithValue(":author", "The Dev");
+        cmd.Parameters.AddWithValue(":title", "Cats: So many!");
+        cmd.Parameters.AddWithValue(":content", "There are so many cats where I live. Loads of them!");
+        cmd.Parameters.AddWithValue(":timestamp", now);
+        cmd.Parameters.AddWithValue(":likes", 3);
+ 
+        await cmd.ExecuteNonQueryAsync();
     }
 }
 
