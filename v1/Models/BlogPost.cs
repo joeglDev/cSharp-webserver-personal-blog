44ad6653092e09bf2585ffc163a7559b5c8d3dda
Joe Gilbert (He/Him) <104070284+joeglDev@users.noreply.github.com>
2024-10-12 18:23:18 +0100

    3-env-variables (#4)
    Added environment variables for database connection string

--- v1/Models/BlogPost.cs
+++ v1/Models/BlogPost.cs
@@ -1,46 +1,47 @@
 namespace Blog.Models;
 
 
- public class LikedByItem
-    {
-        public int LikedByItemId { get; set; } // Primary key
-      //  public int UserId { get; set; }
-        public string? UserName { get; set; }
+public class LikedByItem
+{
+  public int LikedByItemId { get; set; } // Primary key
+                                         //  public int UserId { get; set; }
+  public string? UserName { get; set; }
 
-          // Parameterless constructor for Entity Framework
-    public LikedByItem()
-    {
-    }
+  // Parameterless constructor for Entity Framework
+  public LikedByItem()
+  {
+  }
 
-        public LikedByItem(string name) {
-          UserName = name;
-        }
-    }
+  public LikedByItem(string name)
+  {
+    UserName = name;
+  }
+}
 
 public class BlogPostItem
 {
 
-    public int Id { get; set; }
-    public string Author { get; set; } = "unassigned author";
-    public string Title { get; set; } = "unassigned title";
-    public string Content { get; set; } = "unassigned content";
+  public int Id { get; set; }
+  public string Author { get; set; } = "unassigned author";
+  public string Title { get; set; } = "unassigned title";
+  public string Content { get; set; } = "unassigned content";
 
-    public DateTime TimeStamp {get; set;}
+  public DateTime TimeStamp { get; set; }
 
-   public List<LikedByItem> Likes {get; set;} = new List<LikedByItem>();
+  public List<LikedByItem> Likes { get; set; } = new List<LikedByItem>();
 
-      // Parameterless constructor for Entity Framework
-    public BlogPostItem()
-    {
-    }
+  // Parameterless constructor for Entity Framework
+  public BlogPostItem()
+  {
+  }
 
-    // Constructor
-    public BlogPostItem(int id, string author, string title, string content)
-    {
-        Id = id;
-        Author = author;
-        Title = title;
-        Content = content;
-    }
+  // Constructor
+  public BlogPostItem(int id, string author, string title, string content)
+  {
+    Id = id;
+    Author = author;
+    Title = title;
+    Content = content;
+  }
 
 }
